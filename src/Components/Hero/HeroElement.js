7a71fcc6c94f13a29e8cf02f7318e3051347a965
Mohammad <mohammedsheikhkhamis@gmail.com>
2022-06-27 09:55:43 +0300

    new

--- src/Components/Hero/HeroElement.js
+++ src/Components/Hero/HeroElement.js
@@ -1,96 +1,82 @@
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import ImgBg from '../../images/Pizza.jpg';
-export const HeroContainer =styled.div`
-    background: linear-gradient(to right , rgba(0,0,0,0.7),
-    rgba(0,0,0,0.1)), 
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import ImgBg from "../../images/Pizza.jpg";
+export const HeroContainer = styled.div`
+  background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
     url(${ImgBg});
-    height: 100vh;
-    background-position:center;
-    background-size: cover;
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
 `;
 
 export const HeroContent = styled.div`
-    height: calc(100vh - 80px);
-    max-height:100%;
-    width : 100%;
-    padding: 0rem calc((100vw - 1300px )/ 2);
+  height: calc(100vh - 80px);
+  max-height: 100%;
+  width: 100%;
+  padding: 0rem calc((100vw - 1300px) / 2);
 `;
 
 export const HeroItems = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    height: 100vh;
-    max-height: 100%;
-    padding: 0 2rem;
-    width:650px;
-    color: #fff;
-    text-transform: uppercase;
-    line-height: 1;
-    font-weight: bold;
-    @media screen and (max-width : 650px){
-        width: 100%;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0 2rem;
+  width: 650px;
+  color: #fff;
+  text-transform: uppercase;
+  line-height: 1;
+  font-weight: bold;
+  @media screen and (max-width: 650px) {
+    width: 100%;
+  }
 `;
 export const HeroH1 = styled.h1`
-    font-size: clamp(2.5rem , 10vw , 5rem);
-    margin-bottom:1rem;
-    box-shadow : 3px 5px #e9ba23;
-    letter-spacing: 3px;
+  font-size: clamp(2.5rem, 10vw, 5rem);
+  margin-bottom: 1rem;
+  box-shadow: 3px 5px #e9ba23;
+  letter-spacing: 3px;
 `;
 
 export const HeroP = styled.p`
-    font-size : clamp(2rem ,2.5vw , 3rem);
-    margin-bottom: 2rem;
-
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  margin-bottom: 2rem;
 `;
-export const ButtonGroup =styled.div`
-    display: flex;
-    align-items: center;
-    justify-content:space-between;
-
+export const ButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
 `;
 export const SignUp = styled(Link)`
-    font-size: 1.4rem;
-    padding: 1rem 4rem;
-    border:none;
-    text-decoration:none;
-    list-style:none;
-    border-radius: 6px;
-    background : #e31837;
-    color : #fff;
-    transition:0.2s ease-out;
+  text-decoration: none;
+  background: #e31837;
+  color: #fff;
+  padding: 10px;
+  border-radius: 15rem;
+  transition: 0.2s ease-out;
 
+  &:hover {
+    background: #ffc500;
 
-
-    &:hover {
-        background : #ffc500;
-        transition: 0.2s ease-out;
-        letter-spacing:2px;
-        cursor: pointer;
-        color: #000;
-    }
+    letter-spacing: 2px;
+    cursor: pointer;
+    color: #000;
+  }
 `;
 export const SignIn = styled(Link)`
-    margin-left: 20px;
-    font-size: 1.4rem;
-    padding: 1rem 4rem;
-    border:none;
-    text-decoration:none;
-    list-style:none;
-    border-radius: 6px;
-    background : #e31837;
-    color : #fff;
-    transition:0.2s ease-out;
+  text-decoration: none;
+  background: #e31837;
+  color: #fff;
+  padding: 10px;
+  border-radius: 15rem;
+  transition: 0.2s ease-out;
 
-
-    &:hover {
-        background : #ffc500;
-        transition: 0.2s ease-out;
-        letter-spacing:2px;
-        cursor: pointer;
-        color: #000;
-    }
+  &:hover {
+    background: #ffc500;
+    transition: 0.2s ease-out;
+    letter-spacing: 2px;
+    cursor: pointer;
+    color: #000;
+  }
 `;
--- src/Components/sign/signElement.js
+++ src/Components/sign/signElement.js
@@ -1,39 +1,34 @@
-import styled from 'styled-components';
-import Image from '../../images/product6.jpg';
+import styled from "styled-components";
+import Image from "../../images/product6.jpg";
 export const SignContainer = styled.div`
-    background: linear-gradient(to right , rgba(0,0,0,0.7),
-    rgba(0,0,0,0.1)), 
+  background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
     url(${Image});
-    height: 100vh;
-    background-position:center;
-    background-size: cover;
-
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
 `;
 export const SignWrapper = styled.div`
-    display : flex ; 
-    flex-wrap : wrap ;
-    justify-content: center;
-    margin: 0 auto;
-    
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 export const SignHeader = styled.h1`
-    font-size : clamp(2rem , 2.5vw , 3rem);
-    text-align : center;
-    margin-top: 20px;
-    /* margin-bottom : 5rem ; */
-    color:#fff;
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  text-align: center;
+  margin-top: 10px;
+  /* margin-bottom : 5rem ; */
+  color: #fff;
 `;
 
 export const SignCard = styled.div`
-    margin:auto;
-    margin-top: 25px;
-    background-color: #f9f9f9;
-    border-radius: 9px;
-    line-height: 2;
-    width : 300px;
+  margin: auto;
+
+  background-color: #f9f9f9;
+  border-radius: 9px;
+  line-height: 2;
+  width: 300px;
 `;
-export const SignIcon =styled.div`
-    padding-left: 50px;
-    padding-top:30px;
-    
+export const SignIcon = styled.div`
+  padding-left: 50px;
+  padding-top: 30px;
 `;
